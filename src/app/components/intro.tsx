c1844fc36a8b0053fdbd4ecdab9b8ae59337bef2
Dimitris Giannoulis <dimitrisgiannoulis.jg@gmail.com>
2024-02-07 15:08:00 +0000

    Adds intro animations

--- src/app/components/intro.tsx
+++ src/app/components/intro.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
-import intro from './intro.module.css';
+import animate from './css/animations.module.css';
 
 export default function Intro() {
   return (
@@ -8,13 +8,13 @@ export default function Intro() {
         <div className="flex items-center justify-center">
             <div className='relative'>
                 <Image 
-                    className='h-24 w-24 rounded-full  w-[7rem] h-[7rem] border p-[0.2rem] shadow-sm transition-shadow shadow-[#6394bf] rounded-full' 
+                    className='h-24 w-24 rounded-full w-[7rem] h-[7rem] border p-[0.2rem] shadow-sm transition-shadow shadow-[#6394bf] rounded-full' 
                     src="/prof-pic-linkedin.jpeg" 
                     alt="Profile Picture of Dimitris Giannoulis." 
                     height={192} width={192}
                     priority={true}
                 />
-                <span className={`${intro.picEmoji} ${intro.greetAnimation}  absolute bottom-[0rem] right-[0.2rem] text-4xl`}>👋</span>
+                <span className={`${animate.animate} ${animate.animationGreet} absolute bottom-[0rem] right-[0.2rem] text-4xl`}>👋</span>
             </div>
         </div>
     </section>
