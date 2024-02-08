f88d417921b170ceea674472df0b4809bcea7ae7
Dimitris Giannoulis <dimitrisgiannoulis.jg@gmail.com>
2024-02-08 00:22:14 +0000

    Adds intro text, buttons, and starter animations

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,8 +4,7 @@ import React from 'react'
 // import { motion } from 'framer-motion'
 import { links } from '@lib/data';
 import Link from 'next/link';
-import header from './css/header.module.css';
-import { before } from 'node:test';
+import animate from './css/animations.module.css';
 
 
 export default function Header() {
@@ -13,7 +12,7 @@ export default function Header() {
     // <header className='z-[999] relative'>
     //     <div className="fixed top-0 left-1/2 h-[4.5rem] -translate-x-1/2 w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"></div>
     // </header>
-    <header className={`${header.header} z-[999] relative`}>
+    <header className={`${animate.animate} ${animate.headerDown} ${animate.anmDur1} ${animate.anmDel04}} ${animate.anmFillBoth} z-[999] relative`}>
         <div className="fixed top-0 w-[100vw] h-14 rounded-none bg-white shadow-md shadow-black/5 border border-white border-opacity-40 bg-opacity-80 backdrop-blur-[0.5rem] sm:top-5 sm:h-[3.20rem] sm:w-[36rem] left-1/2 -translate-x-1/2 sm:rounded-full"
         ></div>
 
--- src/app/components/intro.tsx
+++ src/app/components/intro.tsx
@@ -1,22 +1,60 @@
-import Image from 'next/image'
-import React from 'react'
-import animate from './css/animations.module.css';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { BsArrowRight } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import animate from "./css/animations.module.css";
 
 export default function Intro() {
   return (
-    <section>
-        <div className="flex items-center justify-center">
-            <div className='relative'>
-                <Image 
-                    className='h-24 w-24 rounded-full w-[7rem] h-[7rem] border p-[0.2rem] shadow-sm transition-shadow shadow-[#6394bf] rounded-full' 
-                    src="/prof-pic-linkedin.jpeg" 
-                    alt="Profile Picture of Dimitris Giannoulis." 
-                    height={192} width={192}
-                    priority={true}
-                />
-                <span className={`${animate.animate} ${animate.animationGreet} absolute bottom-[0rem] right-[0.2rem] text-4xl`}>👋</span>
-            </div>
+    <section className="mt-[0.3rem] sm:mt-[2rem]">
+      {/* Image */}
+      <div
+        className={`${animate.animate} ${animate.zoomIn} ${animate.anmDur04} flex items-center justify-center`}
+      >
+        <div className="relative">
+          <Image
+            className="w-[7rem] h-[7rem] border p-[0.2rem] shadow-sm transition-shadow shadow-[#6394bf] rounded-full"
+            src="/prof-pic-linkedin.jpeg"
+            alt="Profile Picture of Dimitris Giannoulis."
+            height={192}
+            width={192}
+            priority={true}
+          />
+          <span
+            className={`${animate.animate} ${animate.animationGreet} absolute bottom-[0rem] right-[0.2rem] text-4xl`}
+          >
+            👋
+          </span>
         </div>
+      </div>
+
+      {/* Hero text */}
+      <p
+        className={`${animate.animate} ${animate.up} ${animate.anmDur04} text-center pt-2 text-3xl sm:text-4xl ps-10 pe-10 max-w-[50rem] font-medium !leading-[1.5]`}
+      >
+        <span className="font-bold">Hello, I'm Dimitris.</span> A{" "}
+        <span className="font-bold">Front End Developer</span> with a passion
+        for creating dynamic web applications.
+      </p>
+
+      {/* Buttons */}
+      <div
+        className={`${animate.animate} ${animate.up} ${animate.anmDur04} ${animate.anmDel04} ${animate.anmFillBck} flex items-center justify-center gap-1 sm:gap-3 mt-[1rem]`}
+      >
+        <Link
+          className="bg-gray-900 text-white shadow-md shadow-[#ffffff] px-7 py-3 flex items-center gap-2 rounded-full"
+          href="#contact"
+        >
+          Contact me here <BsArrowRight />
+        </Link>
+        <a
+          className="bg-white text-gray-900 px-7 py-3 flex items-center gap-2 rounded-full shadow-sm shadow-[#00000071]"
+          href=""
+        >
+          Download CV <HiDownload />{" "}
+        </a>
+      </div>
     </section>
-  )
+  );
 }
