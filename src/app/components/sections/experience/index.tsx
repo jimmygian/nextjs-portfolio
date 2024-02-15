39b268c8ca922624e841da7d6fd978e6d36d661e
Dimitris Giannoulis <dimitrisgiannoulis.jg@gmail.com>
2024-02-15 18:02:07 +0000

    Creates basic styling of exp icons

--- src/app/components/sections/experience/index.tsx
+++ src/app/components/sections/experience/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import SectionHeader from '@app/components/sections/section-header';
 import exp from './exp.module.css';
+import Image from 'next/image';
+import graduateIcon from '@public/icons/graduate-cap-svgrepo-com.svg'
 
 export default function Experience() {
   return (
@@ -11,14 +13,20 @@ export default function Experience() {
             </div> */}
             
             <div className={`${exp.container} ${exp.leftContainer}`}>
+                <div className={`${exp.iconContainer}`}>
+                    <Image className={`${exp.icon}`} src={graduateIcon} alt="isadfasdg"/>
+                </div>
                 <article className={`${exp.textBox}`}>
                         <h2 className='text-xl'>FED Bootcamp</h2>
                         <small>2023 - 2024</small>
                         <p>Rigorous 4-month Front End Bootcamp bla bla Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab autem illum quo rerum alias nemo dolor. </p>
                 </article>
             </div>
-            
+
             <div className={`${exp.container} ${exp.rightContainer}`}>
+                <div className={`${exp.iconContainer}`}>
+                    <Image className={`${exp.icon}`} src={graduateIcon} alt="isadfasdg"/>
+                </div>
                 <article className={`${exp.textBox}`}>
                         <h2 className='text-xl'>FED Bootcamp</h2>
                         <small>2023 - 2024</small>
