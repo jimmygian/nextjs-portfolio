8af477eb8d77e6c505c986793b7f4bcce0e1d15f
Dimitris Giannoulis <dimitrisgiannoulis.jg@gmail.com>
2024-02-17 00:18:03 +0000

    Fixes experience section responsiveness

--- src/app/components/sections/experience/index.tsx
+++ src/app/components/sections/experience/index.tsx
@@ -3,32 +3,28 @@ import SectionHeader from "@app/components/sections/section-header";
 import exp from "./exp.module.css";
 import ExpCard from "./components/expCard";
 import { experiencesData } from "@lib/data";
-import ExpCardDialogContextProvider from "@app/context/dialog-context";
 
 export default function Experience() {
 
   const expCount = experiencesData.length;
-  
   return (
-    <ExpCardDialogContextProvider>
-      <section id="experience" className="mb-28 scroll-mt-28 sm:mb-40 text-center ">
-        <SectionHeader>Experience</SectionHeader>
-        <div className={`${exp.timeline}`}>
-          
-          {/* Line */}
-          <div className={`${exp.line}`} style={{height: `${(12 * expCount)}rem`}}></div>
+    <section id="experience" className="mb-28 scroll-mt-28 sm:mb-40 text-center flex flex-col justify-center">
+      <SectionHeader>Experience</SectionHeader>
+      <div className={`${exp.timeline}`}>
+        
+        {/* Line */}
+        <div className={`${exp.line}`} style={{height: `${(12 * expCount)}rem`}}></div>
 
-          {/* EXPERIENCE Cards Mapping */}
-          {experiencesData.map((expData, index) => {
-            let side: string = "right";
-            if (index % 2 === 0) {
-              side = "left";
-            }
-            return <ExpCard side={side} data={expData} />;
-          })}
+        {/* EXPERIENCE Cards Mapping */}
+        {experiencesData.map((expData, index) => {
+          let side: string = "right";
+          if (index % 2 === 0) {
+            side = "left";
+          }
+          return <ExpCard side={side} data={expData} />;
+        })}
 
-        </div>
-      </section>
-    </ExpCardDialogContextProvider>
+      </div>
+    </section>
   );
 }
