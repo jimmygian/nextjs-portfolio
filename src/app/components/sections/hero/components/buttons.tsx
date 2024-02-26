843e12a08a01ced388b13d47c5563b8f2527fcc1
Dimitris Giannoulis <dimitrisgiannoulis.jg@gmail.com>
2024-02-26 00:18:53 +0000

    Separates hero buttons to a component

---
+++ src/app/components/sections/hero/components/buttons.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import React, { useRef } from "react";
+import animate from '@app/css/animations.module.css'
+import { useIntersectionObserver } from "@lib/hooks";
+import { useGlobalContext } from "@app/context/global-context";
+import { BsArrowRight, BsGithub, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import Link from "next/link";
+
+
+type ButtonsType = {
+  inView?: boolean;
+  shown?: boolean;
+  absolute?: boolean;
+  top?: string;
+  bottom?: string;
+  right?: string;
+  left?: string;
+  className?: string;
+}
+
+export default function Buttons({inView, shown, absolute, className, top, bottom, left, right}: ButtonsType) {
+
+  const { setActiveSection, setTimeOfLastClick } = useGlobalContext()
+
+
+  return (
+    <div
+      className={`
+        
+        ${
+          absolute
+            ? `sm:hidden absolute bottom-[${bottom ? bottom : `20%`}] right-[${right ? right : `7%`}] top-[${top ? top : ``}] left-[${left ? left : ``}]`
+            : `hidden sm:flex`
+        }
+        ${
+          className ? className : ""
+        }
+        ${
+          inView
+            ? `${animate.animate} ${animate.up} ${animate.anmDur07} ${animate.anmDel07} ${animate.anmFillBck}`
+            : "opacity-0"
+        }
+        flex 
+        flex-wrap
+        justify-end
+        self-end
+        sm:self-center
+        gap-3 
+        max-w-[75%]
+      `}
+    >
+      <Link
+        className="!text-sm sm:text-base px-3 py-2 sm:px-7 sm:py-3 group xs-styles bg-zinc-900 text-white shadow-md shadow-black/10 flex items-center gap-2 rounded-full outline-none focus:scale-105 hover:scale-110 active:scale-105 transition dark:shadow-black/10 dark:bg-gray-600"
+        href="#contact"
+        onClick={() => {
+          setActiveSection("Contact");
+          setTimeOfLastClick(Date.now());
+        }}
+      >
+        Contact me here{" "}
+        <BsArrowRight className="opacity-70 group-hover:translate-x-2 transition" />
+      </Link>
+      <a
+        className="!text-sm sm:text-base px-3 py-2 sm:px-7 sm:py-3 group xs-styles bg-white text-zinc-900 flex items-center gap-2 rounded-full shadow-sm shadow-[#00000071] outline-none focus:scale-105 hover:scale-110 active:scale-105 transition dark:bg-white/10 dark:text-white/90 dark:shadow-md"
+        href="/CV-ph.pdf"
+        download={true}
+      >
+        Download CV{" "}
+        <HiDownload className="opacity-70  group-hover:scale-125 transition" />
+      </a>
+      <div className="flex gap-2">
+        <a
+          href="https://linkedin.com/jimmygian"
+          target="_blank"
+          className="p-[0.8rem] sm:p-4  text-sm sm:text-base bg-white text-zinc-700 flex items-center gap-2 rounded-full shadow-sm shadow-[#000000a4] outline-none focus:scale-[1.15] hover:scale-110 active:scale-[1.15] transition dark:bg-white/10 dark:text-white/60 dark:shadow-md"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://github.com/jimmygian"
+          target="_blank"
+          className="p-[0.8rem] sm:p-4  text-sm sm:text-base bg-white  text-zinc-700 flex items-center gap-2 rounded-full shadow-sm shadow-[#000000a4] outline-none focus:scale-[1.15] hover:scale-110 active:scale-[1.15] transition dark:bg-white/10 dark:text-white/60 dark:shadow-md"
+        >
+          <BsGithub />
+        </a>
+      </div>
+    </div>
+  );
+}
--- src/app/components/sections/hero/index.tsx
+++ src/app/components/sections/hero/index.tsx
@@ -3,6 +3,7 @@
 import animate from "@app/css/animations.module.css";
 import backgroundLight from "@public/images/bgs/abstract-envelope-light.svg";
 import backgroundDark from "@public/images/bgs/abstract-envelope-dark.svg";
+import Buttons from "./components/buttons";
 import { BsArrowRight, BsLinkedin, BsGithub } from "react-icons/bs";
 import hero from "./hero.module.css";
 import { HiDownload } from "react-icons/hi";
@@ -43,6 +44,8 @@ export default function HeroNew() {
         className={`${hero.heroImg}`}
       />
 
+      <Buttons inView={inView} absolute={true}/>
+
       {/* Hero */}
       <div
         className={`${hero.hero} flex flex-col items-center justify-center mt-[2rem] xs:mt-[4rem] sm:mt-[30vh] px-4 xs:px-5 xs:mx-5 relative`}
@@ -65,7 +68,8 @@ export default function HeroNew() {
               className={`
                   ${
                     inView
-                      ? `${animate.animate} ${animate.left} ${animate.anmDur04} ${animate.anmDel01} ${animate.anmFillBck}` : "opacity-0"
+                      ? `${animate.animate} ${animate.left} ${animate.anmDur04} ${animate.anmDel01} ${animate.anmFillBck}`
+                      : "opacity-0"
                   }
                   text-end 
                   font-extralight
@@ -92,7 +96,7 @@ export default function HeroNew() {
             overflow-hidden
           `}
         >
-          <div 
+          <div
             className={`
               border-l border-[--cp1-black2] dark:border-[--cp1-white1]
               ${
@@ -110,6 +114,9 @@ export default function HeroNew() {
                   ps-5
                   text-md
                   xs:text-xl
+                  mb-[3rem]
+                  xs:mb-[10rem] 
+                  sm:mb-[7rem] 
                   ${
                     inView
                       ? `${animate.animate} ${animate.right} ${animate.anmDur05} ${animate.anmDel03} ${animate.anmFillBck}`
@@ -121,62 +128,10 @@ export default function HeroNew() {
               passion for creating dynamic web applications.
             </h2>
           </div>
-          </div>
-
-        {/* Buttons */}
-        <div
-          className={`
-              text-xl
-              ${
-                inView
-                  ? `${animate.animate} ${animate.up} ${animate.anmDur07} ${animate.anmDel07} ${animate.anmFillBck}`
-                  : "opacity-0"
-              }
-              flex 
-              flex-wrap
-              justify-end
-              gap-3 
-              mt-[6rem]
-              xs:mt-[10rem] 
-              sm:mt-[7rem] 
-            `}
-        >
-          <Link
-            className="group xs-styles bg-zinc-900 text-white shadow-md shadow-black/10 px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-105 hover:scale-110 active:scale-105 transition dark:shadow-black/10 dark:bg-gray-600"
-            href="#contact"
-            onClick={() => {
-              setActiveSection("Contact");
-              setTimeOfLastClick(Date.now());
-            }}
-          >
-            Contact me here{" "}
-            <BsArrowRight className="opacity-70 group-hover:translate-x-2 transition" />
-          </Link>
-          <a
-            className="group xs-styles bg-white text-zinc-900 px-7 py-3 flex items-center gap-2 rounded-full shadow-sm shadow-[#00000071] outline-none focus:scale-105 hover:scale-110 active:scale-105 transition dark:bg-white/10 dark:text-white/90 dark:shadow-md"
-            href="/CV-ph.pdf"
-            download={true}
-          >
-            Download CV{" "}
-            <HiDownload className="opacity-70  group-hover:scale-125 transition" />
-          </a>
-          <div className="flex gap-2">
-            <a
-              href="https://linkedin.com/jimmygian"
-              target="_blank"
-              className="bg-white p-4 text-zinc-700 flex items-center gap-2 rounded-full shadow-sm shadow-[#000000a4] outline-none focus:scale-[1.15] hover:scale-110 active:scale-[1.15] transition dark:bg-white/10 dark:text-white/60 dark:shadow-md"
-            >
-              <BsLinkedin />
-            </a>
-            <a
-              href="https://github.com/jimmygian"
-              target="_blank"
-              className="bg-white p-4 text-zinc-700 flex items-center gap-2 rounded-full shadow-sm shadow-[#000000a4] outline-none focus:scale-[1.15] hover:scale-110 active:scale-[1.15] transition dark:bg-white/10 dark:text-white/60 dark:shadow-md"
-            >
-              <BsGithub />
-            </a>
-          </div>
         </div>
+
+        <Buttons inView={inView}/>
+
       </div>
     </section>
   );
