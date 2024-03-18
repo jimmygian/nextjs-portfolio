c399e542a705aff8f0045a7fd3d56eab4d4dd09e
Dimitris Giannoulis <dimitrisgiannoulis.jg@gmail.com>
2024-03-18 12:18:38 +0000

    Creates sticky sections

--- src/app/components/sections/section-header.tsx
+++ src/app/components/sections/section-header.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] });
+const poppins = Poppins({ subsets: ['latin'], weight: "100" });
+const poppinsbold = Poppins({ subsets: ['latin'], weight: "300" });
 
 type SectionHeadingProps = {
     children: React.ReactNode;
@@ -11,9 +14,10 @@ type SectionHeadingProps = {
 
 export default function SectionHeader({ title, children }: SectionHeadingProps) {
   return (
-    <>
-        <h2 className={`${inter.className} text-3xl font-medium capitalize mb-8`}>{children}</h2>
-        { title && <h2 className='text-3xl font-medium capitalize mb-8'>{title}</h2>}
-    </>
+    <div className='h-fit flex justify-center sticky top-[0%] pt-[3.8rem] bg-white z-50 text-center'>
+        {/* <h2 className={`${poppins.className} absolute translate-x-[0.15rem] text-[--cp1-color1] top-[0.15rem] opacity-30 text-xl capitalize font-semibold tracking-wide scale-y-[1] scale-x-[0.9] mb-8`}>{children}</h2> */}
+        <h2 className={`${poppins.className} text-xl capitalize font-extralight tracking-wide scale-y-[1] scale-x-[0.9] mb-8`}>{children}</h2>
+        { title && <h2 className='text-xl uppercase font-extralight mb-8'>{title}</h2>}
+    </div>
   )
 }
